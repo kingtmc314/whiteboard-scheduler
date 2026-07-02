7997e2c59b4af4ca53cab53e8cf265deeb27d1e0
kingtmc314 <kingsleytsemc314@gmail.com>
2026-07-02 14:06:05 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "電子白板施工避開日子 2025-2026",
+  description: "中學課室每日使用情況可視化",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-HK">
+      <body>{children}</body>
+    </html>
+  );
+}
